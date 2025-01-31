3bfd9492d220db0474edff030bcf8c8feced55e7
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2025-01-31 10:53:12 +0600

    learned error handling by adding not fount page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+
+const ErrorPage = () => {
+  return (
+    <div>
+        <p>ErrorPage</p>
+        <Link href={"/"}>Go back to home</Link>
+    </div>
+  )
+}
+
+export default ErrorPage
