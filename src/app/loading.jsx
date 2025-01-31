b56514ac3983ca4258ae6eaa5814af6e2fd08501
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2025-01-31 11:02:16 +0600

    learned loading state by adding loading.jsx

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+
+
+const loading = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center"><span className="loading loading-spinner text-success"></span></div>
+  )
+}
+
+export default loading
